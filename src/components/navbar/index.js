f90866c15363e03b625380cb5acb54f0e0288bc4
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-02 13:09:14 +0530

    part one of responsiveness done.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import { NavLink } from "react-router-dom";
-
+import { slide as Menu } from "react-burger-menu";
 import styled from "styled-components";
 import tw from "twin.macro";
 import LogoImage from "../../assets/logo.svg";
+
+var styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    right: "36px",
+    top: "36px",
+  },
+  bmBurgerBars: {
+    background: "#fff",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#373a47",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+};
 const NavContainer = styled.div`
   ${tw`
  
@@ -25,6 +71,28 @@ text-3xl
 `;
 
 export const NavBar = () => {
+  const isMobile = useMediaQuery({ maxWidth: "640px" });
+
+  if (isMobile) {
+    return (
+      <>
+        <NavLink to="/">
+          <Logo src={LogoImage} />
+        </NavLink>
+        <Menu right styles={styles}>
+          <NavLink to="/blogs">
+            <NavLinkComponent>Blogs</NavLinkComponent>
+          </NavLink>
+          <NavLink to="/about">
+            <NavLinkComponent>About</NavLinkComponent>
+          </NavLink>
+          <NavLink to="/contact">
+            <NavLinkComponent>Contact me</NavLinkComponent>
+          </NavLink>
+        </Menu>
+      </>
+    );
+  }
   return (
     <>
       <NavContainer>
