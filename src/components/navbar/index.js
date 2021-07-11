36914e8723aaa77ba77396b496a465870d0f8b37
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-12 00:10:21 +0530

    Making navbar responsive

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,46 +1,180 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import LogoImage from "../../assets/logo.svg";
-const NavContainer = styled.div`
+const NavContainer = styled.nav`
   ${tw`
- 
-  justify-around
-  items-center
-  flex
-    w-1/2
-    h-24
-`};
+bg-dark-blue
+`}
 `;
 
-const Logo = styled.img``;
-
-const NavLinkComponent = styled.h3`
+const NavContainerOuter = styled.div`
   ${tw`
-text-white
-text-3xl
-`};
+  max-w-7xl mx-auto px-4 sm:px-6 lg:px-8
+`}
+`;
+
+const NavContainerInner = styled.div`
+  ${tw`
+
+flex items-center justify-between h-16
+`}
+`;
+
+const NavItemsContainer = styled.div`
+  ${tw`
+flex items-center
+`}
+`;
+
+const NavLogoContainer = styled.div`
+  ${tw`
+  flex-shrink-0
+`}
+`;
+
+const NavLinksMDContainer = styled.div`
+  ${tw`
+  hidden md:block
+`}
+`;
+
+const NavLinksMDContainerInner = styled.div`
+  ${tw`
+  hidden md:block
+`}
+`;
+
+const NavOpenCloseButtonContainer = styled.div`
+  ${tw`
+mr-2 flex md:hidden
+`}
+`;
+
+const NavOpenCloseButton = styled.button`
+  ${tw`
+  bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white
+`}
+`;
+
+const NavTransitionContainer = styled.div`
+  ${tw`
+md:hidden
+`}
+`;
+
+const NavReferenceContainer = styled.div`
+  ${tw`
+  px-2 pt-2 pb-3 space-y-1 sm:px-3
+`}
 `;
 
 export const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <NavContainer>
-        <NavLink to="/">
-          <Logo src={LogoImage} />
-        </NavLink>
+        <NavContainerOuter>
+          <NavContainerInner>
+            <NavItemsContainer>
+              <NavLogoContainer>
+                <img className="h-8 w-8" src={LogoImage} />
+              </NavLogoContainer>
+              <NavLinksMDContainer>
+                <NavLinksMDContainerInner>
+                  <a
+                    href="/blogs"
+                    className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-4xl">
+                    Blogs
+                  </a>
 
-        <NavLink to="/blogs">
-          <NavLinkComponent>Blogs</NavLinkComponent>
-        </NavLink>
-        <NavLink to="/about">
-          <NavLinkComponent>About</NavLinkComponent>
-        </NavLink>
-        <NavLink to="/contact">
-          <NavLinkComponent>Contact me</NavLinkComponent>
-        </NavLink>
+                  <a
+                    href="#"
+                    className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-4xl">
+                    About
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-4xl">
+                    Contact me
+                  </a>
+                </NavLinksMDContainerInner>
+              </NavLinksMDContainer>
+            </NavItemsContainer>
+            <NavOpenCloseButtonContainer>
+              <NavOpenCloseButton
+                onClick={() => setIsOpen(!isOpen)}
+                type="button">
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </NavOpenCloseButton>
+            </NavOpenCloseButtonContainer>
+          </NavContainerInner>
+        </NavContainerOuter>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95">
+          {(ref) => (
+            <NavTransitionContainer>
+              <NavReferenceContainer ref={ref}>
+                <a
+                  href="#"
+                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
+                  Blogs
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                  About
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+                  Contact me
+                </a>
+              </NavReferenceContainer>
+            </NavTransitionContainer>
+          )}
+        </Transition>
       </NavContainer>
     </>
   );
